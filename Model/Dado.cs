997414e4bc3917bd1692a55a14f6e45103bfc2be
wellingtonbsz <32886317+Tatobsz@users.noreply.github.com>
2018-06-02 20:32:04 -0300

    Add files via upload

---
+++ Model/Dado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaAurora
+{
+    class Dado
+    {
+        
+        public int[] Faces { get; set; } = new int[5];
+        public int[] FaceSequencia { get; set; } = new int[5];
+        public string[] FaceDadoImg { get; set; } = new string[6];
+        Random random = new Random();
+
+        public Dado()
+        {
+            FaceDadoImg[0] = "C: \\Users\\wesle\\OneDrive\\Área de Trabalho\\SoftwareAurora\\Resources\\Dado1.jpeg";
+
+            FaceDadoImg[1] = "C:\\Users\\wesle\\OneDrive\\Área de Trabalho\\SoftwareAurora\\Resources\\Dado2.jpeg";
+
+            FaceDadoImg[2] = "C:\\Users\\wesle\\OneDrive\\Área de Trabalho\\SoftwareAurora\\Resources\\Dado3.jpeg";
+
+            FaceDadoImg[3] = "C:\\Users\\wesle\\OneDrive\\Área de Trabalho\\SoftwareAurora\\Resources\\Dado4.jpeg";
+
+            FaceDadoImg[4] = "C:\\Users\\wesle\\OneDrive\\Área de Trabalho\\SoftwareAurora\\Resources\\Dado5.jpeg";
+
+            FaceDadoImg[5] = "C:\\Users\\wesle\\OneDrive\\Área de Trabalho\\SoftwareAurora\\Resources\\Dado6.jpeg";
+        }        
+
+        public int RodarDados()
+        {
+            return random.Next(1, 7);            
+        }        
+    }
+}
---
+++ Model/Jogador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaAurora
+{
+    class Jogador
+    {
+        public string Nome { get; set; }
+        public int Pontos { get; set; }
+
+        public Jogador()
+        {
+            Nome = "Jogador*";
+            Pontos = 0;
+        }
+
+        public void somarPontos(int pontos)
+        {
+            Pontos += pontos;            
+        }
+    }
+}
